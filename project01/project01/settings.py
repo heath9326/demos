43fb9c2221883730f045e0ffb5841b8d59b2cb61
heath9326 <heath9326@yandex.ru>
2023-06-26 11:46:55 +0300

    Doecker does not see host

--- project01/project01/settings.py
+++ project01/project01/settings.py
@@ -80,23 +80,23 @@ WSGI_APPLICATION = 'project01.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'db',
-#        'USER': 'admin',
-#        'PASSWORD': 'admin123!',
-#        'HOST': 'db',
-#        'PORT': '5433',
-#    }
-#}
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db',
+        'USER': 'admin',
+        'PASSWORD': 'admin123!',
+        'HOST': 'db',
+        'PORT': '5432',
+    } 
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
