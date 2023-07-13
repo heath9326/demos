6f55188fb9f04f146a89a99f38d433853b997e28
heath9326 <heath9326@yandex.ru>
2023-07-13 18:04:19 +0300

    get/post work, delete doe not

--- project02/accounts/urls.py
+++ project02/accounts/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 
 # Imports
-from .views import GetCustomUsersView
+from .views import GetCustomUsersView, CustomUserListAPIView, SingleUserRetrieveView, get_users, test
+
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('getusers', GetCustomUsersView.as_view(), name='getusers'),
+    path('getusersapi', GetCustomUsersView.as_view(), name='getusersapi'),
+    path('getuserapi/<int:pk>', GetCustomUsersView.as_view(), name='getusersapi'),
+    path('getusersdecorator', get_users, name='getusersdecorator'),
+    path('getuserslist', CustomUserListAPIView.as_view(),name='getuserslist'),
+    path('getsingleuser/<int:pk>', SingleUserRetrieveView.as_view(), name='getsingleuser'),
+    path('test/<int:pk>/', test, name='test'),
+    # path('testkwargs', testkwargs, name='testkwargs'),
 ]
--- project02/accounts/views.py
+++ project02/accounts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Project specific:
 from .models import CustomUser
@@ -6,13 +7,79 @@ from .serializers import CustomUserSerializer
 
 # REST
 from rest_framework.response import Response
+from rest_framework.decorators import api_view, schema
 from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework import generics as drf_generics
 
 
-# Create your views here.
+
+# Through class view
 class GetCustomUsersView(APIView):
+    serializer_class = CustomUserSerializer
+
     def get(self, request):
         queryset = CustomUser.objects.all().order_by('id')
-        serializer = CustomUserSerializer(instanse=queryset, many=True)
+        serializer = CustomUserSerializer(instance=queryset, many=True)
 
         return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = CustomUserSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+            # How do I reaload the page
+        else:
+            return Response(serializer.errors, status=400)
+        
+    def delete(self, request, pk):
+        user = CustomUser.objects.filter(id=pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
+# Through function-based view using a decorator
+@api_view(['GET', 'POST'])
+def get_users(request):
+    # Does not render and HTTP 
+    renderer_classes = [CustomUserSerializer]
+    
+    if request.method == 'GET':
+        queryset = CustomUser.objects.all().order_by('id')
+        serializer = CustomUserSerializer(instance=queryset, many=True)
+
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = CustomUserSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=400)
+    else:
+        return Response({'error': 'Type of request is not handled yet!'}, status=400)
+    
+
+# This view functions as an alternative to APIView get
+class CustomUserListAPIView(drf_generics.ListAPIView):
+    serializer_class = CustomUserSerializer
+    queryset = CustomUser.objects.all()
+
+class SingleUserRetrieveView(drf_generics.RetrieveAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+
+
+
+
+
+# Test Views
+def test(request, pk):
+    id = pk
+    return HttpResponse('ID is %s' % id)
+
+# def testkwargs(request):
+#     id = request.query_params('id')
+#     return HttpResponse('ID is %s' % id)
