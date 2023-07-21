f1d73fe912211468061b665a77a6443b0b3754a0
Evgeniya Seminenko <heath9326@yandex.ru>
2023-07-21 17:20:30 +0300

    POST APIView does not show foreign fields

--- project02/articles/models.py
+++ project02/articles/models.py
@@ -2,16 +2,31 @@ from django.db import models
 from authors.models import Author
 from accounts.models import CustomUser
 
+from common.mixins import TimeStampedMixin
+from common.models import TimeStampedModel, SlugFieldModel
 
-class Article(models.Model):
+# Tools
+from django.template.defaultfilters import slugify
+
+
+class Article(TimeStampedModel, SlugFieldModel):
     title = models.CharField(max_length=150)
     text = models.TextField(blank=False)
-    authors = models.ManyToManyField(Author)
+    authors = models.ManyToManyField(Author, related_name='articles', blank=False)
 
     objects = models.Manager()
 
     def __str__(self):
         return f'Article №{self.id}: "{self.title}"'
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            #FIXME: ignores russian
+            # Newly created object, so set slug
+            self.slug = slugify(self.title)
+
+        super(Article, self).save(*args, **kwargs)
+
     
 class Comment(models.Model):
     text = models.TextField(max_length=300)
