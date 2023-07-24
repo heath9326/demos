3bd77b885f38b238653f102d5df820e01723d943
Evgeniya Seminenko <heath9326@yandex.ru>
2023-07-24 10:05:47 +0300

    working on custom create method

--- project02/articles/serializers.py
+++ project02/articles/serializers.py
@@ -2,12 +2,31 @@ from rest_framework import serializers
 
 from .models import Article, Comment
 
-class ArticleSerializer(serializers.ModelSerializer):
+class ArticleGetSerializer(serializers.ModelSerializer):
+    comments = serializers.StringRelatedField(many=True)
+
     class Meta:
-        comments = serializers.StringRelatedField(many=True)
         model = Article
-        fields = ['title', 'text', 'authors', 'comments']
+        fields = ['id', 'title', 'text', 'authors', 'comments', 'created_at', 'updated_at', 'slug']
         depth = 1
+
+class ArticleSetSerializer(serializers.ModelSerializer):
+    """
+    {
+        'title': 'title",
+        'text': 'text',
+        'authors': [
+            1, 3, 5, 10
+        ]
+    }
+    """
+    class Meta:
+        model = Article
+        fields = ['title', 'text', 'authors']
+
+    #TODO: def create, где авторы = список авторов из validated data,  потом for author in authors(Author.)
+
+#FIXME: Doest not regect post form if black=False fields are empty:
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
